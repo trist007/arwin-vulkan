6578e2633732ddaf5726cc25ce415c0bd771cd00
Tristan <trist007@darkterminal.net>
2026-04-07 19:45:54 -0500

    added some new includes

--- arwin/code/build.bat
+++ arwin/code/build.bat
@@ -3,16 +3,20 @@
 echo Compilation started at %date% %time%
 echo,
 
+REM LIBRARIES
 set SDL_LIB=..\arwin\external\SDL3-3.4.2\lib\x64\SDL3.lib
 set SDLIMAGE_LIB=..\arwin\external\SDL3_image-3.4.0\lib\x64\SDL3_image.lib
 set SDLTTF_LIB=..\arwin\external\SDL3_ttf-3.2.2\lib\x64\SDL3_ttf.lib
+
+REM INCLUDES
 set SDL_Include=/I"..\arwin\external\SDL3-3.4.2\include"
 set SDLIMAGE_Include=/I"..\arwin\external\SDL3_image-3.4.0\include"
 set SDLTTF_Include=/I"..\arwin\external\SDL3_ttf-3.2.2\include"
+set FMT_Include=/I"..\arwin\external\fmt-12.1.0\include"
 
 set VULKAN_INCLUDE=/I"..\arwin\code"
 
-set CommonCompilerFlags=-MT -nologo -fp:fast -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4244 -wd4996 -wd4456 -FC -Z7 %SDL_Include% %SDLIMAGE_Include% %SDLTTF_Include% %VULKAN_INCLUDE%
+set CommonCompilerFlags=/utf-8 /std:c++20 -MT -nologo -fp:fast -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4244 -wd4996 -wd4456 -FC -Z7 %SDL_Include% %SDLIMAGE_Include% %SDLTTF_Include% %VULKAN_INCLUDE% %FMT_Include%
 
 set CommonLinkerFlags=-incremental:no -opt:ref /DEBUG /PDB:main.pdb %SDL_LIB% %SDLIMAGE_LIB% %SDLTTF_LIB%
 
