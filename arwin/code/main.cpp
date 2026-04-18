40404300a0d08aedf569c0af2e529e30a6861710
trist007 <trist007@darkterminal.net>
2026-04-18 17:02:27 -0500

    using new functions from tutorial

--- arwin/code/main.cpp
+++ arwin/code/main.cpp
@@ -9,5 +9,6 @@ int main(int argc,  char *argv[])
 
     runVulkanEngine(&engine);
 
-    cleanupVulkanEngine(&engine);
+    howtoCleanupVulkanEngine(&engine);
+    //cleanupVulkanEngine(&engine);
 }
